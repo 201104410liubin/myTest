79c67cffd9b844ea53e3970e5d467419bf0d76c6
Jiang <jiang@JiangdeMac-mini.local>
2016-10-28 11:45:45 +0800

    新增了newFile文件

---
+++ myFirstTest/newFile.h
@@ -0,0 +1,13 @@
+//
+//  newFile.h
+//  myFirstTest
+//
+//  Created by Jiang on 16/10/28.
+//  Copyright © 2016年 yingfu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface newFile : NSObject
+
+@end
---
+++ myFirstTest/newFile.m
@@ -0,0 +1,13 @@
+//
+//  newFile.m
+//  myFirstTest
+//
+//  Created by Jiang on 16/10/28.
+//  Copyright © 2016年 yingfu. All rights reserved.
+//
+
+#import "newFile.h"
+
+@implementation newFile
+
+@end
