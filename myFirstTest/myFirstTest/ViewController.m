b1519051d1ca15ba6d54d53168ebbd589c502084
Jiang <jiang@jiangdemac-mini.local>
2016-10-28 11:35:43 +0800

    初始化项目

---
+++ myFirstTest/myFirstTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  myFirstTest
+//
+//  Created by Jiang on 16/10/28.
+//  Copyright © 2016年 yingfu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
